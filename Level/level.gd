0edd06bd59baf45e36a3f83a6ea633c13591e251
Enter-Name <Enter-Name@outlook.com>
2026-05-07 23:22:37 -0400

    Fixed Fail issues

--- Level/level.gd
+++ Level/level.gd
@@ -1,5 +1,1 @@
 extends Node2D
-
-
-func _on_player_fail() -> void:
-	get_tree().change_scene_to_file("res://Non-Game-Scenes/Fail.tscn");
--- Player/player.gd
+++ Player/player.gd
@@ -4,9 +4,9 @@ const SPEED = 450
 const JUMP_VELOCITY = -750
 const GRAVITY = 1500
 
-signal fail;
-
 @onready var sprite = $AnimatedSprite2D
+
+@onready var fail = preload("res://Non-Game-Scenes/Fail.tscn")
 
 func _physics_process(delta):
 	if not is_on_floor():
@@ -30,4 +30,4 @@ func _physics_process(delta):
 	for i in get_slide_collision_count():
 		var collision = get_slide_collision(i);
 		if collision.get_collider().is_in_group("enemies"):
-			fail.emit();
+			get_tree().change_scene_to_packed(fail);
